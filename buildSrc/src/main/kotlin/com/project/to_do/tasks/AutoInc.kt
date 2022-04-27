7a8b35ecd14b67069c14565a032414253debf7fe
olegm <olegmojegov@yandex.ru>
2022-04-27 23:51:22 +0200

    test15

--- buildSrc/src/main/kotlin/com/project/to_do/tasks/AutoInc.kt
+++ buildSrc/src/main/kotlin/com/project/to_do/tasks/AutoInc.kt
@@ -69,7 +69,7 @@ open class AutoInc : DefaultTask() {
         result = "add \n " + Git.runCommand("git add version/version.properties")
         println(result)
         println("status " + Git.runCommand("git status"))
-        result =  "commit \n " + Git.gitCommit("version/version.properties","\"autoInc version code: $major.$minor - $build\"")
+        result =  "commit \n " + Git.gitCommit(versionHelper.fullPath(),"\"autoInc version code: $major.$minor - $build\"")
         println(result)
         println("status " + Git.runCommand("git status"))
         result = "push \n " + Git.runCommand(
