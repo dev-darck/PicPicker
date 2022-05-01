c49ee9213888011179287a29ebf66d749b44e6bc
denis <denisbeck555@gmail.com>
2022-05-01 16:46:14 +0300

    fix signed artifacts workflows

--- buildSrc/src/main/kotlin/com/project/to_do/extensions/ProjectExt.kt
+++ buildSrc/src/main/kotlin/com/project/to_do/extensions/ProjectExt.kt
@@ -7,7 +7,7 @@ import com.project.to_do.storage.ReleaseSigningProperties
 import org.gradle.api.Project
 
 fun Project.getSigningProperties(signingName: String) = when (signingName) {
-    debug -> DebugSigningProperties(rootDir.path)
-    release -> ReleaseSigningProperties(rootDir.path)
+    debug -> DebugSigningProperties(projectDir.path)
+    release -> ReleaseSigningProperties(projectDir.path)
     else -> throw IllegalArgumentException("Unknown signingName = $signingName")
 }
