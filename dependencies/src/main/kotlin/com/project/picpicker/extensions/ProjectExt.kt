a99e990385a24b79e1ff2c3c2b8a5dd68767d74c
olegm <olegmojegov@yandex.ru>
2022-05-05 23:30:07 +0200

    example Composite builds

--- dependencies/src/main/kotlin/com/project/picpicker/extensions/ProjectExt.kt
+++ dependencies/src/main/kotlin/com/project/picpicker/extensions/ProjectExt.kt
@@ -1,9 +1,9 @@
 package com.project.picpicker.extensions
 
 import com.project.picpicker.debug
-import com.project.picpicker.release
 import com.project.picpicker.managers.DebugSigningPropertiesManager
 import com.project.picpicker.managers.ReleaseSigningPropertiesManager
+import com.project.picpicker.release
 import org.gradle.api.Project
 
 fun Project.getSigningProperties(signingName: String) = when (signingName) {
