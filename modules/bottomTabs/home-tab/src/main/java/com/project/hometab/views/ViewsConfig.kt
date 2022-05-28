a3d68281574a8fea2a45664389173b34ff9806e7
denis <denisbeck555@gmail.com>
2022-05-28 16:44:19 +0300

    issue#114 add scrollableTabBar

---
+++ modules/bottomTabs/home-tab/src/main/java/com/project/hometab/views/ViewsConfig.kt
@@ -0,0 +1,15 @@
+package com.project.hometab.views
+
+object ViewsConfig {
+    private const val spacingToText = 40
+    private const val spacingBetweenTabs = 20
+
+    const val tabHeight = 50
+
+    const val tabHorizontalPaddings = spacingBetweenTabs / 2
+    const val tabRowHorizontalPaddings = spacingToText - tabHorizontalPaddings
+    const val indicatorHorizontalPaddings = tabHorizontalPaddings
+    const val dividerHorizontalPaddings = spacingToText
+
+    const val animationDurationMillis = 250
+}
---
+++ modules/scrollable-tab-row/build.gradle.kts
@@ -0,0 +1,12 @@
+
+import com.project.picpicker.Dependency.composePreview
+import com.project.picpicker.Dependency.jetpackComposeUi
+import com.project.picpicker.dependency.helper.addDep
+import com.project.picpicker.plugins.config.module
+
+module(
+    appDependency = addDep(
+        *jetpackComposeUi,
+        *composePreview,
+    )
+)
