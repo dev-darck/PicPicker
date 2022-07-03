4aa8769f17ce7b9b2c8bb351748e6b82691503f9
Oleg Mojegov <olegmojegov@yandex.ru>
2022-07-03 22:11:05 +0200

    issue #40 fix update version

--- dependencies/src/main/kotlin/com/project/picpicker/checker/model/Version.kt
+++ dependencies/src/main/kotlin/com/project/picpicker/checker/model/Version.kt
@@ -3,4 +3,5 @@ package com.project.picpicker.checker.model
 data class Version(
     val oldVersion: String = "",
     var newVersion: String = "",
+    val tomlVersion: TomlLibVersion = TomlLibVersion()
 )
